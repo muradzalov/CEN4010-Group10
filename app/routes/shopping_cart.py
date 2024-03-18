40c1f47e857014a12bb766ac6e22257e20c19310
mward046 <133927715+mward046@users.noreply.github.com>
2024-03-18 16:01:08 -0400

    Adding comments to logic to explain how code works

--- app/routes/shopping_cart.py
+++ app/routes/shopping_cart.py
@@ -7,6 +7,7 @@ from ..schemas import ShoppingCart as ShoppingCartSchema, CartItem
 
 router = APIRouter()
 
+#Functionality: Add a book to the shopping cart.
 @router.post("/shopping-cart/{user_id}/add", response_model=ShoppingCartSchema)
 async def add_book_to_cart(user_id: int, item: CartItem, db: Session = Depends(get_db)):
     user = db.query(UserModel).filter(UserModel.user_id == user_id).first()
@@ -30,7 +31,7 @@ async def add_book_to_cart(user_id: int, item: CartItem, db: Session = Depends(g
     db.refresh(cart_item)
 
     return cart
-
+#Functionality: Retrieve the subtotal price of all items in the user’s shopping cart.
 @router.get("/shopping-cart/{user_id}/subtotal", response_model=float)
 async def get_cart_subtotal(user_id: int, db: Session = Depends(get_db)):
     cart = db.query(ShoppingCartModel).filter(ShoppingCartModel.user_id == user_id).first()
@@ -40,6 +41,7 @@ async def get_cart_subtotal(user_id: int, db: Session = Depends(get_db)):
     subtotal = sum(item.quantity * item.book.price for item in cart.cart_items)
     return subtotal
 
+#Functionality: Retrieve the list of book(s) in the user’s shopping cart.
 @router.get("/shopping-cart/{user_id}/items", response_model=List[CartItem])
 async def get_cart_items(user_id: int, db: Session = Depends(get_db)):
     cart = db.query(ShoppingCartModel).filter(ShoppingCartModel.user_id == user_id).first()
@@ -48,6 +50,7 @@ async def get_cart_items(user_id: int, db: Session = Depends(get_db)):
 
     return cart.cart_items
 
+#Functionality: Delete a book from the shopping cart instance for that user.
 @router.delete("/shopping-cart/{user_id}/remove/{item_id}", response_model=ShoppingCartSchema)
 async def remove_book_from_cart(user_id: int, item_id: int, db: Session = Depends(get_db)):
     cart = db.query(ShoppingCartModel).filter(ShoppingCartModel.user_id == user_id).first()
