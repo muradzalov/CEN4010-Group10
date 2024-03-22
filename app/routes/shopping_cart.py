9801e01bc2bf6d9c7e713f3a044e5c3ebeeb4c39
mward046 <133927715+mward046@users.noreply.github.com>
2024-03-21 20:55:48 -0400

    updating the cart retrieval to provide more useful information

--- app/routes/shopping_cart.py
+++ app/routes/shopping_cart.py
@@ -58,7 +58,14 @@ async def get_cart_items(user_id: int, db: Session = Depends(get_db)):
     if not cart:
         raise HTTPException(status_code=404, detail="Shopping cart not found")
 
-    return cart.cart_items
+    cart_items = []
+    for item in cart.cart_items:
+        book = db.query(BookModel).filter(BookModel.id == item.book_id).first()
+        if book:
+            cart_item = CartItem(book=book, quantity=item.quantity)
+            cart_items.append(cart_item)
+
+    return cart_items
 
 # Functionality: Delete a book from the shopping cart instance for that user
     # Logic: Given a book Id and a User Id, remove the book from the user’s shopping cart
