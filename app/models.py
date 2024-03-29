1d82fe13e47eaab066009fc18811d2d2c211aaf1
queatle <queatlewheatle@gmail.com>
2024-03-29 15:56:45 -0400

    Reverted schemas.py and profiles.py to ensure non-breaking merging to main

--- app/models.py
+++ app/models.py
@@ -14,24 +14,3 @@ class Book(Base):
     copies_sold = Column(Integer)
     price = Column(Float)
     publisher = Column(String, index=True)
-
-
-# SQLAlchemy model for the "profile" table
-class Profile(Base):
-    __tablename__ = "profiles"
-
-    pusername = Column(String, index=True)
-    pname = Column(String, index=True)
-    pemail = Column(String, index=True)
-    paddress = Column(String, index=True)
-    ppassword = Column(String, primary_key=True, index=True)
-
-
-class CreditCard(Base):
-    __tablename__ = "creditcard"
-
-    cusername = Column(String, index=True)
-    ccreditcard = Column(Integer, primary_key=True)
-    csc = Column(Integer)
-    czipcode = Column(Integer)
-    cexpdate = Column(Integer)
